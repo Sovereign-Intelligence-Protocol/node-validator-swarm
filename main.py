ad0453de51055460f110d9490cbe9f547ad8ae06
Joshua Kleinsasser <kleinsasserjoshua3@gmail.com>
2026-04-27 06:41:38 -0500

    Fix indentation and remove duplicate scan_for_leads call

--- main.py
+++ main.py
@@ -218,4 +218,6 @@ if __name__ == "__main__":
     
     # Start Scalper Loop
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(scalper.scan_for_leads()
+    loop.run_until_complete(scalper.scan_for_leads())
+
+# --- END OF S.I.P. v5.5 SOURCE --
